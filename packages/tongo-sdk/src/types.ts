5779c8f01626b7d993901f38c810b1613f380f5b
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2025-05-15 22:40:11 -0300

    feat: TongoAddres and utils to convert it

---
+++ packages/tongo-sdk/src/types.ts
@@ -0,0 +1,1 @@
+export type TongoAddress = string & { __type: "tongo" }
--- packages/tongo-sdk/src/utils.ts
+++ packages/tongo-sdk/src/utils.ts
@@ -1,14 +1,28 @@
 import { base58 } from "@scure/base";
 import { bytesToHex } from "@noble/hashes/utils";
 import { ProjectivePoint } from "@scure/starknet";
+import { TongoAddress } from "./types.js";
 
-export function pubKeyAffineToBase58(pub: { x: bigint, y: bigint; }): string {
+// assumes compressed format
+export function pubKeyAffineToHex(pub: { x: bigint, y: bigint; }): string {
     const point = new ProjectivePoint(pub.x, pub.y, 1n);
-    return base58.encode(point.toRawBytes(true))
+    return bytesToHex(point.toRawBytes(true));
 }
 
-export function pubKeyBase58ToAffine(b58string: string): { x: bigint, y: bigint } {
+// assumes compressed format
+export function pubKeyAffineToBase58(pub: { x: bigint, y: bigint; }): TongoAddress {
+    const point = new ProjectivePoint(pub.x, pub.y, 1n);
+    return base58.encode(point.toRawBytes(true)) as TongoAddress;
+}
+
+// assumes compressed format
+export function pubKeyBase58ToAffine(b58string: string): { x: bigint, y: bigint; } {
     const bytes = base58.decode(b58string);
-    return ProjectivePoint.fromHex(bytesToHex(bytes))
+    return ProjectivePoint.fromHex(bytesToHex(bytes));
 }
 
+// assumes compressed format
+export function pubKeyBase58ToHex(b58string: string): string {
+    const bytes = base58.decode(b58string);
+    return bytesToHex(bytes);
+}
