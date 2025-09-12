562bcc98d9a98721ccbe467e80cbbdc071feef6c
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2025-09-11 23:12:21 -0300

    feat: split tests in integration vs unit. Added scripts for devnet setup

--- packages/tongo-sdk/vitest.config.ts
+++ packages/tongo-sdk/vitest.config.ts
@@ -5,13 +5,30 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
-    include: ["test/**/*.test.ts"],
     exclude: ["node_modules", "dist"],
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
       exclude: ["node_modules", "dist", ...configDefaults.exclude],
     },
+    projects: [
+      {
+        // will inherit options from this config like plugins and pool
+        extends: true,
+        test: {
+          name: "unit",
+          include: [ "test/unit/*" ],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          testTimeout: 0,
+          name: "integration",
+          include: [ "test/integration/*" ],
+        },
+      },
+    ]
   },
   resolve: {
     alias: {
