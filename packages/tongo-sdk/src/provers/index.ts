cb216b09f7e16c1b1bfc39403ce657f3b4e9281c
bezze <bezze@users.noreply.github.com>
2025-10-08 15:16:45 -0300

    Delete packages/tongo-sdk/src/provers/index.ts

--- packages/tongo-sdk/src/provers/index.ts
+++
@@ -1,7 +0,0 @@
-// export * from "./audit.js";
-// export * from "./fund.js";
-// export * from "./operation.js";
-// export * from "./ragequit.js";
-// export * from "./rollover.js";
-// export * from "./transfer.js";
-// export * from "./withdraw.js";
