3ff5da09a2d2fb9f5d4a36da84a7fa6fcc4f01dd
Francisco Bezzecchi <bezze@fatsolutions.xyz>
2026-04-14 20:48:30 -0300

    feat: updated withdraw to use new type inference and prefix comp

--- packages/tongo-sdk/src/operations/withdraw.ts
+++ packages/tongo-sdk/src/operations/withdraw.ts
@@ -1,10 +1,17 @@
-import { ProjectivePoint, RelayData } from "../types";
 import { ProofOfWithdraw } from "../provers/withdraw";
-import { CipherBalance } from "../types.js";
-import { Call, Contract, num, CairoOption } from "starknet";
+import { BigNumberish, Call, Contract, num, CairoOption } from "starknet";
 import { AEBalance } from "../ae_balance.js";
+import { StarkPoint } from "../types.js";
+import { TongoAbiType } from "../abi/abi.types.js";
 import { IOperation, OperationType } from "./operation.js";
 import { Audit } from "./audit.js";
+
+export type WithdrawOptions = TongoAbiType<"tongo::structs::operations::withdraw::WithdrawOptions">;
+
+interface StarkCipherBalance {
+    L: StarkPoint;
+    R: StarkPoint;
+}
 
 export interface IWithdrawOperation extends IOperation {
     type: typeof OperationType.Withdraw;
@@ -13,29 +20,25 @@ export interface IWithdrawOperation extends IOperation {
 /**
  * Represents the calldata of a withdraw operation.
  * @interface WithdrawOpParams
- * @property {ProjectivePoint} from - The Tongo account to withdraw from
+ * @property {StarkPoint} from - The Tongo account to withdraw from
  * @property {bigint} amount - The amount of tongo to withdraw
- * @property {bigint} to - The starknet contract address to send the funds to
+ * @property {BigNumberish} to - The starknet contract address to send the funds to
  * @property {AEBalance} hint - AE encryption of the final balance of the account
  * @property {ProofOfWithdraw} proof - ZK proof for the withdraw operation
  * @property {CairoOption<Audit>} auditPart - Optional Audit to declare the balance of the account after the tx
- * @property {RelayData} relayData - relay data for the operation
+ * @property {CairoOption<WithdrawOptions>} withdraw_options - Options including relay data
  * @property {Contract} Tongo - The Tongo instance to interact with
  */
 interface WithdrawOpParams {
-    from: ProjectivePoint;
-    to: bigint;
-    amount: bigint;
-    auxiliarCipher:CipherBalance;
+    from: StarkPoint;
+    to: BigNumberish;
+    amount: BigNumberish;
+    auxiliarCipher: StarkCipherBalance;
     hint: AEBalance;
     proof: ProofOfWithdraw;
     auditPart: CairoOption<Audit>;
     withdraw_options: CairoOption<WithdrawOptions>;
     Tongo: Contract;
-}
-
-export interface WithdrawOptions {
-    relayData: CairoOption<RelayData>,
 }
 
 //TODO: handle this better, maybe something similar to the cairo contracts
@@ -48,7 +51,7 @@ export function serializeWithdrawOptions(withdraw_options: CairoOption<WithdrawO
         arr.push(1n)
     } else {
         arr.push(0n)
-        arr.push(relayData.unwrap()!.fee_to_sender)
+        arr.push(BigInt(relayData.unwrap()!.fee_to_sender))
     }
     return arr
 }
@@ -56,11 +59,11 @@ export function serializeWithdrawOptions(withdraw_options: CairoOption<WithdrawO
 export class WithdrawOperation implements IWithdrawOperation {
     type: typeof OperationType.Withdraw = OperationType.Withdraw;
     Tongo: Contract;
-    from: ProjectivePoint;
-    to: bigint;
-    amount: bigint;
+    from: StarkPoint;
+    to: BigNumberish;
+    amount: BigNumberish;
     hint: AEBalance;
-    auxiliarCipher: CipherBalance;
+    auxiliarCipher: StarkCipherBalance;
     proof: ProofOfWithdraw;
     auditPart: CairoOption<Audit>;
     withdraw_options: CairoOption<WithdrawOptions>;
